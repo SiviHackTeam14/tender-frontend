437d20d86fa7064d6f5c1d9c4ceab2b936702320
Dat <datnt.tnh23@gmail.com>
2026-09-18 15:37:01 +0200

    Change to right thing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterOutlet],
   selector: 'app-root',
   standalone: true,
   styleUrl: './app.css',
